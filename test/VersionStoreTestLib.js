77a4dc3df99d45f9bb27d2a6921faba60f24371d
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-07-04 13:57:09 +0200

    Add revision info storage

--- test/VersionStoreTestLib.js
+++ test/VersionStoreTestLib.js
@@ -33,17 +33,69 @@ export default class VersionControlTestLib {
                 if (error) throw error;
             }).then(done, done);
         });
+        this._testRevInfo();
         this._testFileNames();
         this._testBlobs();
         this._testMimeTypes();
     }
+
+    _testRevInfo() {
+        let that = this;
+        that._write('should store/read revision information', function(){
+            let content = new Buffer('Hello, world', 'UTF-8');
+            let hash = Digest.digest(content);
+            let nameId = 1;
+            let mimeId = 2;
+            let contentId = 3;
+            let diffId = 4;
+            let stamp = new Date().getTime();
+            let revInfo = {
+                nid : nameId,
+                mid : mimeId,
+                length: content.length,
+                hash: hash,
+                content: contentId,
+                diff: diffId,
+                stamp: stamp
+            };
+            let revId;
+            return Promise.resolve()
+            .then(function(){
+                return that.store.storeRevisionInfo(revInfo).then(function(info){
+                    expect(!!info).to.be(true);
+                    expect(!!info.rid).to.be(true);
+                    revId = info.rid;
+                    expect(info.nid).to.eql(revInfo.nid);
+                    expect(info.mid).to.eql(revInfo.mid);
+                    expect(info.length).to.eql(revInfo.length);
+                    expect(info.hash).to.eql(revInfo.hash);
+                    expect(info.content).to.eql(revInfo.content);
+                    expect(info.diff).to.eql(revInfo.diff);
+                    expect(info.stamp).to.eql(revInfo.stamp);
+                });
+            })
+            .then(function(){
+                return that.store.loadRevisionInfo({rid:revId}).then(function(info){
+                    expect(!!info).to.be(true);
+                    expect(info.rid).to.eql(revId);
+                    expect(info.nid).to.eql(revInfo.nid);
+                    expect(info.mid).to.eql(revInfo.mid);
+                    expect(info.length).to.eql(revInfo.length);
+                    expect(info.hash).to.eql(revInfo.hash);
+                    expect(info.content).to.eql(revInfo.content);
+                    expect(info.diff).to.eql(revInfo.diff);
+                    expect(info.stamp).to.eql(revInfo.stamp);
+                });
+            });
+        });
+    }
     
     _testFileNames() {
         let that = this;
-        it('should store/read file names', function(done){
+        that._write('should store/read file names', function(){
             let paths = ['foo/bar/file.doc', 'foo/bar/toto.pdf', 'foo/about.md'];
             let pathIndex;
-            Promise.resolve()
+            return Promise.resolve()
             .then(function(){
                 return that.store.storePaths({paths}).then(function(info){
                     expect(!!info).to.be(true);
@@ -55,19 +107,18 @@ export default class VersionControlTestLib {
                 return that.store.loadPaths({paths}).then(function(info){
                     expect(info).to.eql(pathIndex);
                 })
-            })
-            .then(done, done);
+            });
         });
     }
     
     _testBlobs(){
         let that = this;
-        it ('should store / load blobs', function(done){
+        that._write('should store / load blobs', function(){
             let str = 'This is a content';
             let content = new Buffer(str, 'UTF-8');
             let hash = Digest.digest(content);
             let id;
-            Promise.resolve()
+            return Promise.resolve()
             
             // Store a blob and check that it is available
             .then(function(){
@@ -93,30 +144,56 @@ export default class VersionControlTestLib {
                 expect(info.content.toString()).to.eql(str);
             })
             
-            // Delete blob 
+            // Delete blob
             .then(function(info){
                 return that.store.deleteBlob({id});
             }).then(function(){
                 return that.store.loadBlob({id});
             }).then(function(info){
                 expect(info).to.be(undefined);
-            }).then(done, done);
+            });
         });
     }
     
     _testMimeTypes(){
         let that = this;
-        it('should return an empty list of mime types', function(done) {
-            that.store.loadMimeTypes().then(function(mimes){
+        that._read('should return an empty list of mime types', function() {
+            return that.store.loadMimeTypes().then(function(mimes){
                 expect(mimes).to.eql({});
-            }).then(done, done);
+            });
         });
-        it('should be able to register new mime types', function(done) {
+        that._read('should be able to register new mime types', function() {
             let list = ['text/plain', 'text/x-diff', 'application/binary'];
-            that.store.storeMimeTypes({
+            let index;
+            return that.store.storeMimeTypes({
                 types: list
             }).then(function(mimes){
                 expect(Object.keys(mimes).sort()).to.eql(list.sort());
+                index = mimes;
+            }).then(function(){
+                return that.store.loadMimeTypes({types: list}).then(function(mimes){
+                    expect(mimes).to.eql(index);
+                });
+            });
+        });
+    }
+    
+    // ------------------------------------------------------------------
+    // Utility test methods
+    
+    _write(msg, f) { 
+        let that = this;
+        it(msg, function(done){
+            that.store.writeTransaction(function(){
+                return f.apply(that);
+            }).then(done, done);
+        });
+    }
+    _read(msg, f) { 
+        let that = this;
+        it(msg, function(done){
+            that.store.readTransaction(function(){
+                return f.apply(that);
             }).then(done, done);
         });
     }
